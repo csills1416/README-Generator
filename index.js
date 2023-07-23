db6144d14dfd8f5c0f2477363ae309c4cd7264ee
CSillus <71670415+csills1416@users.noreply.github.com>
2023-07-23 18:39:45 -0400

    Changed "README.md" to "README-Sample.md". This will stop the script from writing over the README.MD

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function writeToFile(fileName, data) {
 
 function init() {
   inquirer.prompt(questions).then((data) => {
-    writeToFile("README.md", generateMarkdown(data));
+    writeToFile("README-Sample.md", generateMarkdown(data));
   });
 }
 
